36a81bc1ecef62702e224bd2281d6e3808ef0a67
niiquash <qua16006@byui.edu>
2022-11-22 18:20:15 -0700

    fetchApi Challenge completed.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import Form from './Form'
+import List from './List'
 
 function App() {
+
+  // api url
+  const API_URL = 'https://jsonplaceholder.typicode.com/'
+
+  // track state for request type and the items fetched
+  const [reqType, setReqType] = useState('users')
+  const [items, setItems] = useState([])
+
+  // fetch data in useEffect function
+  useEffect(() => {
+
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(`${API_URL}${reqType}`)
+        const data = await response.json()
+        setItems(data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    fetchItems()
+  }, [reqType])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Form
+        reqType={reqType}
+        setReqType={setReqType}
+      />
+      <List items={items} />
     </div>
   );
 }
